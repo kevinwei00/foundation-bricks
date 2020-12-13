9e7126e70618a69bed71648cbe11f12dac7f89f4
Kevin Wei <chenghaowei@gmail.com>
2020-12-12 21:41:21 -0500

    build target umd

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'build'),
-    publicPath: ''
+    publicPath: '',
+    libraryTarget: 'umd'
   },
   module: {
     rules: [
